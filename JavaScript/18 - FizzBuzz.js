efe6b39209374dd25d068772312bf19971123d30
Ash Lee <ash.le3@outlook.com>
2016-02-04 18:34:21 +0000

    Update JavaScript

---
+++ JavaScript/18 - FizzBuzz.js
@@ -0,0 +1,26 @@
+/* JavaScript ========================================================================================
+    Write a function called fizzbuzz that accepts a single argument n.
+    Fizzbuzz should return "Fizz" if n is a multiple of 3, "Buzz" if n is a multiple of 5,
+    and n if n is a multiple of neither 3 nor 5.
+=================================================================================================== */
+
+// Solution
+function fizzbuzz(n) {
+    var output = "";
+    if (n % 3 === 0) output += "Fizz";
+    if (n % 5 === 0) output += "Buzz";
+    return (output ? output : n); 
+}
+
+// Output
+console.log(fizzbuzz(3));
+// => "Fizz"
+
+console.log(fizzbuzz(5));
+// => "Buzz"
+
+console.log(fizzbuzz(15));
+// => "FizzBuzz"
+
+console.log(fizzbuzz(4));
+// => "4"
---
+++ JavaScript/19 - Chess Board.js
@@ -0,0 +1,243 @@
+/* JavaScript =================================================================================================================
+    Create a function which returns a string that represents an 8×8 grid, using newline characters to separate lines.
+    At each position of the grid there is either a space or a “#” character.
+    The characters should resemble a chess board.
+============================================================================================================================ */
+
+
+// Solution 1
+var gridSize = prompt("Size of grid"),
+    chessBoard = "";
+
+for (var line = 0; line < gridSize; line++) {
+  
+  for (var square = 0; square < gridSize; square++) {
+    if ((line + square) % 2 === 0) {
+      chessBoard += "#";
+    } else {
+      chessBoard += " ";
+    }
+  }
+
+  chessBoard += "\n";
+}
+ 
+// Output (8)
+console.log(chessBoard);
+/*
+# # # # 
+ # # # #
+# # # # 
+ # # # #
+# # # # 
+ # # # #
+# # # # 
+ # # # #
+*/
+
+
+
+
+
+// Solution 2 - OOP + Prototypal Inheritance
+function Board(gridSize) {
+  this.size = gridSize;
+  this.grid = "";
+
+  this.createFrame();
+
+  for (var line = 0; line < this.size; line++) {
+    for (var square = 0; square < this.size; square++) {
+      if (square === 0) this.grid += "|";
+
+      if ((line + square) % 2 === 0) {
+        this.grid += "##";
+      } else {
+        this.grid += "  ";
+      }
+
+      if (square === (this.size - 1)) this.grid += "|";
+    }
+
+    this.grid += "\n";
+  }
+
+  this.createFrame();
+}
+
+Board.prototype.createFrame = function() {
+  for (var frame = 0; frame < ((this.size * 2) + 2); frame++) {
+    this.grid += "-";
+  }
+
+  this.grid += "\n";
+};
+
+// Output
+var chessBoard = new Board(8);
+console.log(chessBoard.grid);
+/*
+------------------
+|##  ##  ##  ##  |
+|  ##  ##  ##  ##|
+|##  ##  ##  ##  |
+|  ##  ##  ##  ##|
+|##  ##  ##  ##  |
+|  ##  ##  ##  ##|
+|##  ##  ##  ##  |
+|  ##  ##  ##  ##|
+------------------
+*/
+
+
+
+
+
+// Solution 3 - OOP
+function Board(gridSize) {
+  this.size = gridSize;
+  this.grid = "";
+  
+  this.createFrame = function() {
+    for (var frame = 0; frame < ((this.size * 2) + 2); frame++) {
+      this.grid += "-";
+    }
+
+    this.grid += "\n";
+  };
+  
+  this.createGrid = function() {
+    for (var line = 0; line < this.size; line++) {
+      for (var square = 0; square < this.size; square++) {
+        if (square === 0) this.grid += "|";
+
+        if ((line + square) % 2 === 0) {
+          this.grid += "##";
+        } else {
+          this.grid += "  ";
+        }
+
+        if (square === (this.size - 1)) this.grid += "|";
+      }
+
+      this.grid += "\n";
+    }
+  };
+
+  this.display = function() {
+    return this.grid;
+  };
+  
+  this.createFrame();
+  this.createGrid();
+  this.createFrame();
+}
+
+// Output
+var chessBoard = new Board(8);
+console.log(chessBoard.display());
+/* 
+------------------
+|##  ##  ##  ##  |
+|  ##  ##  ##  ##|
+|##  ##  ##  ##  |
+|  ##  ##  ##  ##|
+|##  ##  ##  ##  |
+|  ##  ##  ##  ##|
+|##  ##  ##  ##  |
+|  ##  ##  ##  ##|
+------------------
+*/
+
+
+
+
+
+// Solution 4 - OOP + IIFE
+function Board(gridSize) {
+  this.size = gridSize;
+  this.grid = "";
+
+  this.createFrame = function() {
+    for (var frame = 0; frame < ((this.size * 2) + 2); frame++) {
+      this.grid += "-";
+    }
+    
+    this.grid += "\n";
+  };
+
+  this.createGrid = function() {
+    for (var line = 0; line < this.size; line++) {
+      for (var square = 0; square < this.size; square++) {
+        if (square === 0) this.grid += "|";
+
+        if ((line + square) % 2 === 0) {
+          this.grid += "##";
+        } else {
+          this.grid += "  ";
+        }
+
+        if (square === (this.size - 1)) this.grid += "|";
+      }
+
+      this.grid += "\n";
+    }
+  };
+
+  this.display = function() {
+    return this.grid;
+  };
+
+  // Work around local scope of IIFE
+  var that = this;
+
+  // Auto construct grid layout with IIFE
+  (function() {
+    that.createFrame();
+    that.createGrid();
+    that.createFrame();
+  })();
+}
+
+// Output
+var chessBoard = new Board(8);
+console.log(chessBoard.display());
+/*
+------------------
+|##  ##  ##  ##  |
+|  ##  ##  ##  ##|
+|##  ##  ##  ##  |
+|  ##  ##  ##  ##|
+|##  ##  ##  ##  |
+|  ##  ##  ##  ##|
+|##  ##  ##  ##  |
+|  ##  ##  ##  ##|
+------------------
+*/
+
+var chessBoard = new Board(20);
+console.log(chessBoard.display());
+/*
+------------------------------------------
+|##  ##  ##  ##  ##  ##  ##  ##  ##  ##  |
+|  ##  ##  ##  ##  ##  ##  ##  ##  ##  ##|
+|##  ##  ##  ##  ##  ##  ##  ##  ##  ##  |
+|  ##  ##  ##  ##  ##  ##  ##  ##  ##  ##|
+|##  ##  ##  ##  ##  ##  ##  ##  ##  ##  |
+|  ##  ##  ##  ##  ##  ##  ##  ##  ##  ##|
+|##  ##  ##  ##  ##  ##  ##  ##  ##  ##  |
+|  ##  ##  ##  ##  ##  ##  ##  ##  ##  ##|
+|##  ##  ##  ##  ##  ##  ##  ##  ##  ##  |
+|  ##  ##  ##  ##  ##  ##  ##  ##  ##  ##|
+|##  ##  ##  ##  ##  ##  ##  ##  ##  ##  |
+|  ##  ##  ##  ##  ##  ##  ##  ##  ##  ##|
+|##  ##  ##  ##  ##  ##  ##  ##  ##  ##  |
+|  ##  ##  ##  ##  ##  ##  ##  ##  ##  ##|
+|##  ##  ##  ##  ##  ##  ##  ##  ##  ##  |
+|  ##  ##  ##  ##  ##  ##  ##  ##  ##  ##|
+|##  ##  ##  ##  ##  ##  ##  ##  ##  ##  |
+|  ##  ##  ##  ##  ##  ##  ##  ##  ##  ##|
+|##  ##  ##  ##  ##  ##  ##  ##  ##  ##  |
+|  ##  ##  ##  ##  ##  ##  ##  ##  ##  ##|
+------------------------------------------
+*/
